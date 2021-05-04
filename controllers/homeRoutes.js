ef956f36999041e5c8cf9584843fa62e69aac079
Jakob Knicley <knicleyjakob@gmail.com>
2021-05-04 14:38:05 -0400

    added session login check to homepage, previously forgot

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,9 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('homePage');
+    res.render('homePage', {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
