08877e77c382852ab6f76726f1a1bcecb3a5f287
Mike Bussert <mbussert@gmail.com>
2021-05-04 19:37:40 -0400

    Updated dashboard

--- public/js/theme.js
+++ public/js/theme.js
@@ -18,6 +18,7 @@ if (
     window.matchMedia('(prefers-color-scheme: dark)').matches)
 ) {
   document.querySelector('html').classList.add('dark');
+  
 } else if (localStorage.theme === 'dark') {
   document.querySelector('html').classList.add('dark');
 }
