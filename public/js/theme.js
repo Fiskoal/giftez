1ce631cc2e5532fb408838dabe2ac6d250c3e7d1
Mike Bussert <mbussert@gmail.com>
2021-05-05 16:55:52 -0400

    Updated dashboard page

--- public/js/theme.js
+++ public/js/theme.js
@@ -10,14 +10,18 @@ document.getElementById('switchTheme').addEventListener('click', function () {
   }
 });
 
-if (
-  localStorage.theme === 'dark' ||
-  (!'theme' in localStorage &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.querySelector('html').classList.add('dark');
-  document.getElementById('switchTheme').checked = true;
-} else if (localStorage.theme === 'dark') {
-  document.querySelector('html').classList.add('dark');
-  document.getElementById('switchTheme').checked = true;
+function loadTheme() {
+  if (
+    localStorage.theme === 'dark' ||
+    (!'theme' in localStorage &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.querySelector('html').classList.add('dark');
+    document.getElementById('switchTheme').checked = true;
+  } else if (localStorage.theme === 'dark') {
+    document.querySelector('html').classList.add('dark');
+    document.getElementById('switchTheme').checked = true;
+  }
 }
+
+window.onload = loadTheme();
