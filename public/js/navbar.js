8cfc668850f94349860347a6bc860be7c6ab4395
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-06 19:54:22 -0400

    more work on toggle

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,5 +1,6 @@
 let toggleBtn = document.querySelector('.toggleNavbar');
 let navbar = document.querySelector('.navbarMenu');
+let btnContainer = document.querySelector('.navbarBtnContainer');
 
 toggleBtn.addEventListener('click', (e) => {
   navbar.classList.toggle('hidden');
