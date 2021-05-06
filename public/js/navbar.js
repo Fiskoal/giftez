46ec30d7182e58d01a7b4cf1f3b5dbfd6660ed85
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-06 19:45:25 -0400

    added toggle js

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,4 +1,7 @@
-  function toggleNavbar(collapseID){
-    document.getElementById(collapseID).classList.toggle("hidden");
-    document.getElementById(collapseID).classList.toggle("flex");
-  }
+let toggleBtn = document.querySelector('.toggleNavbar');
+let navbar = document.querySelector('#navbar');
+
+toggleBtn.addEventListener('click', (e) => {
+  navbar.classList.toggle('hidden');
+  navbar.classList.toggle('flex');
+});
