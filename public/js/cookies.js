df06e9d600cfefae70d24f6c3b60a7ad432031eb
Mike Bussert <mbussert@gmail.com>
2021-05-06 13:17:41 -0400

    Added cookie banner and cookies.js for functionality

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,22 +1,33 @@
-document
-  .getElementById('accept-cookies')
-  .addEventListener('click', function () {
-    let cookieBanner = document.getElementById('cookieBanner');
+// If cookie already exists, don't show banner re: accepting cookies.
+// If no cookie found, show banner.
 
-    localStorage.setItem('cookieSeen', 'shown');
+function showBanner() {
+  return {
+    show: true,
+    cookieName: 'myCookie',
+    setCookie(days = 7) {
+      let d = new Date();
+      d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
+      let expires = 'expires=' + d.toUTCString();
+      document.cookie =
+        this.cookieName + '=' + !this.show + ';' + expires + ';path=/';
 
-    if (localStorage.cookieSeen == 'shown') {
-      cookieBanner.style.display = 'none';
-    }
-  });
-
-function loadCookieBanner() {
-  localStorage.getItem('cookieSeen');
-
-  if (localStorage.cookieSeen === 'shown') {
-    document.getElementById('cookieBanner').style.display = 'none';
-    console.log('Shouldnt show cookie');
-  }
+      this.show = false;
+    },
+    getCookie() {
+      let matches = document.cookie.match(
+        new RegExp(
+          '(?:^|; )' +
+            this.cookieName.replace(
+              /([\.$?*|{}\(\)\[\]\/\+^])/g,
+              '\' ) + "=([^;]*)"'
+            )
+        )
+      );
+      return matches ? decodeURIComponent(matches[1]) : 'true';
+    },
+    checkCookie() {
+      return (this.show = this.getCookie() === 'true');
+    },
+  };
 }
-
-window.onload = loadCookieBanner();
